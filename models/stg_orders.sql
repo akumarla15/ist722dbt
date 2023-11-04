d162dc691dab608d23e613e9e4bc1f2399b8b121
Abhijeet <akumarla@g.syr.edu>
2023-11-04 23:50:31 +0000

    Add customers model, tests, docs.

---
+++ models/stg_orders.sql
@@ -0,0 +1,17 @@
+-- models/stg_orders.sql
+
+{{ config(materialized='view') }}
+
+with orders as (
+
+    select
+        id as order_id,
+        user_id as customer_id,
+        order_date,
+        status
+
+    from {{ source('jaffle_shop', 'orders') }}
+
+)
+
+select * from orders
